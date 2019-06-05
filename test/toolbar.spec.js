7d6ec76b62be3e82d29c3fa946ea9a0a6a300128
Louise Hayes <louise.hayes@workgrid.com>
2019-06-04 23:29:04 -0400

    refactored page objects into seperate pages with base page, login page, toolbar page

--- test/toolbar.spec.js
+++ test/toolbar.spec.js
@@ -1,64 +1,66 @@
 const assert = require('assert');
 require('dotenv').config();
-const Page = require('../lib/Page');
+const LoginPage = require('../lib/LoginPage')
+const ToolBar = require('../lib/ToolBar')
+
 
 describe('Log in / Checks Toolbar rendered & clickable', () => {
     before(async () => {
-        this.page = new Page(browser, process.env.BASEURL)
+        this.loginPage = new LoginPage(browser, process.env.BASEURL)
+        this.toolBar = new ToolBar(browser, process.env.BASEURL)
     });
 
     it('Log In  - Get Host Site Title', async () => {
-        let title = await this.page.navigateToUrl()
+        let title = await this.loginPage.navigateToUrl()
         assert.equal(title, 'Navit Technologies')
-
-        let loginBtn = await this.page.login(process.env.USERNAME, process.env.PASSWORD)
+        let loginBtn = await this.loginPage.login(process.env.USERNAME, process.env.PASSWORD)
         loginBtn.click()
     });
 
     it('checks Host home page is rendered', async () => {
-        let hostPage = await this.page.getHeader()
+        let hostPage = await this.loginPage.getHeader()
         let hostHeader = await hostPage.getText()
         assert.equal(hostHeader.split(' ').slice(0, 3).join(' '), 'Today at Navit')
     })
 
     it('Switch to iframe and Check toolbar renders', async () => {
-        await this.page.getIframe()
-        await this.page.switchToFrame();
-        let toolBar = await this.page.getToolBar()
+        await this.toolBar.getToolBarFrame();
+        await this.toolBar.switchToFrame();
+        let toolBar = await this.toolBar.getToolBar()
         let toolbarText = await toolBar.getText()
         assert.equal(toolbarText, 'To Know\nTo Do\nAssistant\nApps\nActivity\nSettings')
     })
 
     it('clicks To Do & checks the detailed card ', async () => {
-        const todoSummary = await this.page.getDiv('To Do')
-        todoSummary.click()
-        let cardDetail = await this.page.getDetail("You're all done for now. Good work!")
+        const todoSummary = await this.toolBar.getPanel('To Do')
+        await todoSummary.click()
+        let cardDetail = await this.toolBar.getCardSummary("You're all done for now. Good work!")
         let text = await cardDetail.getText()
         assert.equal(text, "You're all done for now. Good work!");
     })
 
     it('clicks To Know & checks the detailed card ', async () => {
-        const todoSummary = await this.page.getDiv('To Know')
+        const todoSummary = await this.toolBar.getPanel('To Know')
         todoSummary.click()
-        let cardDetail = await this.page.getDetail("Meeting")
+        let cardDetail = await this.toolBar.getCardSummary("Meeting")
         let text = await cardDetail.getText()
         assert.equal(text, "MEETING");
     })
 
 
     it('clicks Assistant & checks the detailed card ', async () => {
-        const todoSummary = await this.page.getDiv('Assistant')
-        todoSummary.click()
-        let cardDetail = await this.page.getImage("systemIcon")
-        // console.log("image nane " + cardDetail)
+        const todoSummary = await this.toolBar.getPanel('Assistant')
+        await todoSummary.click()
+        // let cardDetail = await this.toolBar.getImage("systemIcon")
+        // // console.log("image nane " + cardDetail)
         // let text = await cardDetail.getText()
         // assert.equal(text, "systemIcon");
     })
 
     it('clicks Apps & checks the detailed card ', async () => {
-        const todoSummary = await this.page.getDiv('Apps')
-        todoSummary.click()
-        // let cardDetail = await this.page.getDetail("Test Summary Fail")
+        const todoSummary = await this.toolBar.getPanel('Apps')
+        await todoSummary.click()
+        // let cardDetail = await this.toolBar.getDetail("Test Summary Fail")
         // let text = await cardDetail.getText()
         // assert.equal(text, "Test Summary Fail");
     })
